8d20ba1302ec62ea2ecce29a57ebf4ca5e2b969f
jar0m1r <jaromir.vangorsel@gmail.com>
2020-12-15 20:55:25 +0100

    Add StatefulWidget for PlayerInputView Animation

--- lib/ui/secretword/player_input_view.dart
+++ lib/ui/secretword/player_input_view.dart
@@ -1,31 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_calculator/business_logic/secretword/secretword_provider.dart';
+import 'package:riverpod_calculator/business_logic/secretword/secretword_state.dart';
 
-class PlayerInputView extends HookWidget {
+class PlayerInputView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => PlayerInputViewState();
+}
+
+class PlayerInputViewState extends State<PlayerInputView>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  List<String> playerInput = []; //! initial state not like this
+
+  PlayerInputViewState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 100),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
+  _updatePlayerInput(List<String> input) {
+    setState(() {
+      playerInput = input;
+    });
+    _animationController.reset();
+    _animationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final animationController = useAnimationController(
-      duration: Duration(milliseconds: 2000),
-    )..forward();
     final curvedAnimation =
-        CurvedAnimation(parent: animationController, curve: Curves.bounceIn);
-    final gameState = useProvider(secretwordPlayerInputProvider.state);
+        CurvedAnimation(parent: _animationController, curve: Curves.bounceIn);
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: gameState.playerInput
-          .map((letter) => ScaleTransition(
-                scale: Tween<double>(begin: 0.0, end: 1.0)
-                    .animate(curvedAnimation),
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(letter, style: TextStyle(fontSize: 24)),
-                ),
-              ))
-          .toList(),
+    return ProviderListener(
+      provider: secretwordPlayerInputProvider.state,
+      onChange: (context, SecretwordState state) {
+        _updatePlayerInput(state.playerInput);
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: playerInput
+            .map((letter) => ScaleTransition(
+                  scale: Tween<double>(begin: 0.0, end: 1.0)
+                      .animate(curvedAnimation),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text(letter, style: TextStyle(fontSize: 24)),
+                  ),
+                ))
+            .toList(),
+      ),
     );
   }
 }
